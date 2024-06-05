3dd37fc71a70041e64058a4fdb398dbba9877357
Nicolò Pincelli <pincelli.n@gmail.com>
2024-06-05 16:43:57 +0200

    Display ticket

--- js/main.js
+++ js/main.js
@@ -7,14 +7,22 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del bigliett
 //Variabili Globali
 const prezzoKm = 0.21;
 //Creo la variabile del Bottone
+
 const Genera = document.getElementById("work");
+
 console.log(Genera);
 // Creo un evento al click
-Genera.addEventListener("click", function () {
-    // Variabile nome
-    let UserName=document.getElementById("username").value;
 
-    console.log(UserName);
+Genera.addEventListener("click", function () {
+ //Variabile per le classi che devono apparire
+  const ticket = document.getElementById("tik");
+        ticket.classList.remove("d-none");
+
+   // Variabile nome
+  let UserName = document.getElementById("username").value;
+  UserName = UserName.toUpperCase();
+
+  console.log(UserName);
   //Creo varibile km da percorrere
   let Percorso = document.getElementById("userdistance").value;
 
@@ -22,27 +30,27 @@ Genera.addEventListener("click", function () {
   // Calcolo il prezzo del bigletto senza standar
   let priceTicket = prezzoKm * Percorso;
 
-  
   //Creo  variabile età
   let Age = document.getElementById("userage").value;
 
   console.log(Age);
   //Controllo se il soggetto ha diritto ad uno sconto
   if (Age == 2) {
-     priceTicket = priceTicket * 0.4;
-
-    
+    priceTicket = (priceTicket * 0.4).toFixed(2);
+    Age = "Pensionato";
   } else if (Age == 0) {
-    priceTicket = priceTicket * 0.2;
-
-    
+    priceTicket = (priceTicket * 0.2).toFixed(2);
+    Age = "Studente";
+  } else {
+    priceTicket = priceTicket.toFixed(2);
+    Age = "";
   }
+
   console.log(priceTicket);
-
-  document.getElementById("passenger").innerHTML = UserName;
-  document.getElementById("price").innerHTML = priceTicket;
-  return UserName, Age, Percorso, priceTicket;
-
   
+  //stampo nome, prezzo ed offerta
+  document.getElementById("passenger").innerHTML = UserName;
+  document.getElementById("cut").innerHTML = Age;
+  document.getElementById("price").innerHTML = priceTicket + "$";
+  return UserName, Age, Percorso, priceTicket;
 });
-
